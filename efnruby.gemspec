c827a59f47d19fa33d12d1ecb10bb88d1dd8be06
Will Richardson <william.hamish@gmail.com>
2016-02-19 17:06:54 +1300

    Added stupid type checks

--- efnruby.gemspec
+++ efnruby.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'efnruby'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2016-01-23'
   s.summary     = "Makes Ruby efn weird"
   s.description = "Turns functions into blocks and stuff"
--- lib/efnruby.rb
+++ lib/efnruby.rb
@@ -13,17 +13,34 @@ module EfnRuby
         end
       end
   
-      def var(name, *options)
-        if options.include? :get
-          if options.include? :set
-            attr_accessor name
-          else
-            attr_reader name
+      def var(*options)
+        access = options[-3..-1] & [:get, :set]
+        options -= [:get, :set]
+        klass = options.last.is_a?(Class) ? options.delete_at(-1) : BasicObject
+        options.each do |name|
+          create_var(name, access, klass)
+        end
+      end
+      
+      private
+      def create_var(name, access, klass)
+        if !access || access == []
+          access = [:get, :set]
+        end
+        inst = :"@#{name}"
+        if access.include? :get
+          define_method name do
+            instance_variable_get inst
           end
-        elsif options.include? :set
-          attr_writer name
-        else
-          attr_accessor name
+        end
+        
+        if access.include? :set
+          define_method :"#{name}=" do |new_val|
+            unless new_val.is_a? klass
+              raise "Efn error! Can't set #{name} to a #{new_val.class.name}, only #{klass}"
+            end
+            instance_variable_set inst, new_val
+          end
         end
       end
     end
